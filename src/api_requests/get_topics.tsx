29e4f3d2a79b719e6c53a03253f346a9ff79c381
DDstar1 <abhuluimendestiny@gmail.com>
2024-04-05 23:31:42 +0100

    ssdvsd

--- src/api_requests/get_topics.tsx
+++ src/api_requests/get_topics.tsx
@@ -1,7 +1,10 @@
 export default async function getTopics() {
-  const response = await fetch("http://127.0.0.1:8000/api/blog_topics", {
-    cache: "no-cache",
-  });
+  const response = await fetch(
+    "https://oseseo.pythonanywhere.com/api/blog_topics",
+    {
+      cache: "no-cache",
+    }
+  );
 
   if (!response.ok) {
     throw new Error("failed to fetch topics");
